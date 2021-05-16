bae46a318d5e99383a6873ac08eae10bfd244b63
rakeshdama <rakeshd0552@gmail.com>
2021-05-16 12:33:36 +0530

    Difference between Declarative and Imperative Approach

---
+++ src/main/java/imperative/Main.java
@@ -0,0 +1,61 @@
+package imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Person> people = List.of(
+                new Person("RAkesh", Person.Gender.MALE),
+                new Person("No name", Person.Gender.FEMALE),
+                new Person("Name no", Person.Gender.FEMALE)
+        );
+
+        System.out.println("Imperative Approach");
+        // Imperative Approach
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people) {
+            if (Person.Gender.FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+
+            System.out.println(female);
+        }
+
+        System.out.println("\nDeclarative Approach");
+        // Declarative Approach
+       List<Person> females2 = people.stream()
+                .filter(person -> Person.Gender.FEMALE.equals(person.gender))
+                .collect(Collectors.toList());
+
+        System.out.println(females2);
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+
+        enum Gender{
+            MALE, FEMALE
+        }
+    }
+}
