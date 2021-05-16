85322d708bf861bf7cb339ee4cb6edd956c0c22f
rakeshdama <rakeshd0552@gmail.com>
2021-05-16 14:11:27 +0530

    BiFunction

--- src/main/java/functionalInteface/_Function.java
+++ src/main/java/functionalInteface/_Function.java
@@ -1,5 +1,6 @@
 package functionalInteface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
@@ -8,16 +9,29 @@ public class _Function {
     static Function<Integer, Integer> mutitlpeBy10 =
             (number) -> number * 10;
 
+    static BiFunction<Integer, Integer, Integer> numberAdds1andMultiplyByNumFunction = (number, multiplyNum) -> ++number * multiplyNum;
+
+    static int increment(int number) {
+        return ++number;
+    }
+
     public static void main(String[] args) {
+        // Function takes 1 argument and gives 1 result
         System.out.println(increment(8));
         System.out.println(incrementFun.apply(9));
         System.out.println(mutitlpeBy10.apply(2));
         Function<Integer, Integer> add1AndMultiplyBy10 =
                 incrementFun.andThen(mutitlpeBy10);
         System.out.println(add1AndMultiplyBy10.apply(2));
+
+        // BiFunction takes 2 arguments and gives 1 result
+
+        System.out.println(numberAdds1andMultiplyByNum(1, 100));
+        System.out.println(numberAdds1andMultiplyByNumFunction.apply(2, 100));
     }
 
-    static int increment(int number) {
-        return ++number;
+    static int numberAdds1andMultiplyByNum(int numberIncrement, int numberMultiply) {
+        return (numberIncrement + 1) * numberMultiply;
     }
+
 }
