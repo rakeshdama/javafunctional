4719e516b76d8eec275f473145bd070b7032f1ed
rakeshdama <rakeshd0552@gmail.com>
2021-05-18 21:15:16 +0530

    using Supplier FI

--- src/main/java/functionalInteface/_Supplier.java
+++ src/main/java/functionalInteface/_Supplier.java
@@ -1,12 +1,18 @@
 package functionalInteface;
 
+import java.util.function.Supplier;
+
 public class _Supplier {
 
-    public static void main(String[] args) {
-        System.out.println(getDBConnectionUrl());
-    }
+    static Supplier<String> getDBConnectionUrlS = () ->
+            "jdbc://localhost:5432/users";
 
     static String getDBConnectionUrl() {
         return "jdbc://localhost:5432/users";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDBConnectionUrl());
+        System.out.println(getDBConnectionUrlS.get());
     }
 }
