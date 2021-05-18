7a1b1b05714ed724bbe262a674889e68c08b378f
rakeshdama <rakeshd0552@gmail.com>
2021-05-18 21:09:11 +0530

    Predicate FI

--- src/main/java/functionalInteface/_Predicate.java
+++ src/main/java/functionalInteface/_Predicate.java
@@ -6,10 +6,22 @@ public class _Predicate {
 
     static Predicate<String> isPhoneNumberValidP = (phoneNumber) -> phoneNumber.startsWith("07") && phoneNumber.length() == 11;
 
+    static Predicate<String> containsNumber9 = phoneNumber -> phoneNumber.contains("9");
+
     public static void main(String[] args) {
+
+        System.out.println(isPhoneNumberValid("07000000000"));
+        System.out.println(isPhoneNumberValid("00900000000"));
 
         System.out.println(isPhoneNumberValidP.test("07000000000"));
         System.out.println(isPhoneNumberValidP.test("09000000000"));
+
+        System.out.println(containsNumber9.test("07000000000"));
+        System.out.println(containsNumber9.test("09000000000"));
+
+        System.out.println(
+                isPhoneNumberValidP.or(containsNumber9).test("09000000000")
+        );
 
     }
 
